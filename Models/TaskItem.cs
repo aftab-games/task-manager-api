a8e699624d4dec68548d84462d8ae1f1dd394f76
Md. Aftab Uddin <aftab.uddin.games@gmail.com>
2026-07-18 10:08:48 +0600

    Added User model and Linked User via UserId with TaskItem

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -6,5 +6,7 @@
         public string Title { get; set; } = string.Empty;
         public bool IsCompleted { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int UserId { get; set; }
+        public User? User { get; set; }
     }
 }
