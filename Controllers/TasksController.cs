7dab82a352958b76ffc24fd454ed88b43ef03235
Md. Aftab Uddin <aftab.uddin.games@gmail.com>
2026-07-18 12:21:48 +0600

    Authorize attribute added before TaskController class declaration

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TaskManagerApi.Models;
 using TaskManagerApi.Services;
 
@@ -6,6 +7,7 @@ namespace TaskManagerApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class TasksController : ControllerBase
     {
         private readonly ITaskService _taskService;
